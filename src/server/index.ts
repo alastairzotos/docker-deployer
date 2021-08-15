cf16f75d4baf23cd9479403719b828e6ee5cd524
Alastair Zotos <alastairzotos@gmail.com>
2021-08-15 20:26:59 +0300

    use docker api

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,22 +1,56 @@
 import * as express from 'express';
+import { Docker } from 'node-docker-api';
 import { createStorage } from '../storage';
 import { authenticate } from './auth';
-import { spawnProcess } from './proc';
+
+const docker = new Docker({ socketPath: '/var/run/docker.sock' });
+
+const promisifyStream = (stream) => new Promise((resolve, reject) => {
+  stream.on('data', (d) => console.log(d.toString()))
+  stream.on('end', resolve)
+  stream.on('error', reject)
+})
 
 const handleDeploy = async (
   image: string,
   tag: string,
   name: string,
   ports: string,
+  log: (data: string) => void,
 ) => {
-  let logs = [];
+  try {
+    log('Pulling image...');
+    await docker.image.create({}, { fromImage: image, tag })//.then(stream => promisifyStream(stream));
 
-  await spawnProcess('docker', ['pull', `${image}:${tag}`], logs);
-  await spawnProcess('docker', ['stop', name], logs);
-  await spawnProcess('docker', ['rm', name], logs);
-  await spawnProcess('docker', ['run', '-d', '-p', ports, '--name', name, `${image}:${tag}`], logs);
+    const containers = await docker.container.list();
+    const existingContainer = containers.find(container => container.data['Names'][0] === name || container.data['Names'][0] === '/' + name);
 
-  return logs.join('\n');
+    if (!!existingContainer) {
+      log('Removing existing container...');
+      await existingContainer.stop();
+      await existingContainer.delete();
+    }
+
+    const [outPort, inPort] = ports.split(':');
+
+    log('Starting new container...');
+    const container = await docker.container.create({
+      Image: image + ':' + tag,
+      name,
+      PortBindings: {
+        [`${inPort}/tcp`]: [{ HostPort: String(outPort) }]
+      },
+      ExposedPorts: {
+        [`${inPort}/tcp`]: {}
+      }
+    });
+
+    await container.start();
+
+    log('Success');
+  } catch (e) {
+    log(String(e));
+  }
 }
 
 export const startServer = async () => {
@@ -30,12 +64,22 @@ export const startServer = async () => {
   app.post('/deploy', authenticate, async (req, res) => {
     const { image, tag = 'latest', name, ports } = req.body;
 
-    const result = await handleDeploy(image, tag, name, ports);
+    const logs = [];
+    await handleDeploy(
+      image,
+      tag,
+      name,
+      ports,
+      (line: string) => {
+        logs.push(line);
+        console.log(line);
+      }
+    );
 
-    res.json({ response: result });
+    res.json({ response: logs });
   })
 
-  app.listen(port, () => `Listening on http://localhost:${port}`);
+  app.listen(port, () => console.log(`Listening on http://localhost:${port}`));
 };
 
 startServer();
