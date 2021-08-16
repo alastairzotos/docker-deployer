0d9b0e5f05ae81b506558e6916a1d85d5f1d4020
Alastair Zotos <alastairzotos@gmail.com>
2021-08-16 11:36:05 +0300

    fixes

--- server/src/commands/stop.ts
+++ server/src/commands/stop.ts
@@ -1,23 +1,45 @@
 import * as pm2 from 'pm2';
-import { serverProcessName } from "../core";
+import { clientProcessName, serverProcessName } from "../core";
 
-export const handleStop = async () => {
-  pm2.connect(() => {
-    pm2.stop(serverProcessName, error => {
+const deleteProcess = (name: string): Promise<void> =>
+  new Promise((resolve, reject) => {
+    pm2.stop(name, error => {
       if (error) {
-        console.error(error);
-        process.exit(1);
+        return reject(error);
       }
 
-      pm2.delete(serverProcessName, error => {
+      pm2.delete(name, error => {
         if (error) {
-          console.error(error);
-          process.exit(1);
+          return reject(error);
         }
 
-        pm2.disconnect();
-        console.log('Deployment server stopped');
+        resolve();
       })
     })
   })
+
+export const handleStop = async () => {
+  pm2.connect(async () => {
+    await deleteProcess(serverProcessName);
+    await deleteProcess(clientProcessName);
+    pm2.disconnect();
+    console.log('Mission Control stopped');
+  });
+  //   pm2.stop(serverProcessName, error => {
+  //     if (error) {
+  //       console.error(error);
+  //       process.exit(1);
+  //     }
+
+  //     pm2.delete(serverProcessName, error => {
+  //       if (error) {
+  //         console.error(error);
+  //         process.exit(1);
+  //       }
+
+  //       pm2.disconnect();
+  //       console.log('Deployment server stopped');
+  //     })
+  //   })
+  // })
 };
